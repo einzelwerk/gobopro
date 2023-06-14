f93fade500f8e04ea64a893e8c68f4fe43f9cfa9
clutch4t <clutch4t@gmail.com>
2023-06-14 16:41:44 +0300

    Header and Hero update

---
+++ src/scripts/modules/burgerMenu.js
@@ -0,0 +1,18 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu");
+    const openBtn = document.querySelector(".menu__burger");
+    const menuContent = document.querySelector(".menu__content")
+    if (!menu || !openBtn) return;
+
+    openBtn.addEventListener('click', () => {
+        if (menu.classList.contains('menu_active')) {
+            menu.classList.remove('menu_active');
+            openBtn.classList.remove('menu__burger_active');
+            document.body.style.overflow = "auto";
+            return;
+        }
+        menu.classList.add('menu_active');
+        openBtn.classList.add('menu__burger_active');
+        document.body.style.overflow = 'hidden';
+    });
+}
