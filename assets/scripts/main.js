ea3cda6d646890aa89cc941eb35e78ddfcae127b
clutch4t <clutch4t@gmail.com>
2023-07-05 16:40:54 +0300

    Deploy commit current changes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -49,16 +49,19 @@ __webpack_require__.r(__webpack_exports__);
 function toggleMenu() {
   var menu = document.querySelector(".menu");
   var content = document.querySelector(".menu__content");
+  var burgerMenuIcon = document.querySelector('.menu__burger__icon');
   if (!menu || !content) return;
   menu.addEventListener('click', function () {
     if (menu.classList.contains('menu_active')) {
       menu.classList.remove('menu_active');
+      burgerMenuIcon.src = "./assets/images/hamburger-open.svg";
       content.style.display = "none";
       content.style.transform = "translateX(-200px)";
       document.body.style.overflow = "auto";
       return;
     }
     menu.classList.add('menu_active');
+    burgerMenuIcon.src = "./assets/images/burger-close.svg";
     content.style.display = "flex";
     content.style.transform = "translateX(0px)";
     document.body.style.overflow = 'hidden';
