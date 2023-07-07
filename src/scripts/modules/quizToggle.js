b5a1e1b6b6b3261a40d157cc00d964162556565d
clutch4t <clutch4t@gmail.com>
2023-07-07 19:55:33 +0300

    Deploy position of partners heading fix

--- src/scripts/modules/quizToggle.js
+++ src/scripts/modules/quizToggle.js
@@ -2,12 +2,15 @@ export function quizToggle() {
 
     const quiz = document.querySelector(".quiz");
     const quizClose = document.querySelector(".quiz__close");
-    const quizBtn = document.querySelector(".take__quiz");
+    const quizBtn = document.querySelectorAll(".take__quiz");
 
-    quizBtn.addEventListener("click", () => {
-        quiz.classList.remove("inactive")
-        document.body.style.overflow = "hidden"
-    });
+    quizBtn.forEach(btn => {
+        btn.addEventListener("click", () => {
+            quiz.classList.remove("inactive")
+            document.body.style.overflow = "hidden"
+        });
+    })
+
     
     quizClose.addEventListener("click", () => {
         quiz.classList.add("inactive")
