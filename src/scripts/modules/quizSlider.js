d9e0609914ecba3ac0e163fcfd1d0e6ce82029f8
clutch4t <clutch4t@gmail.com>
2023-07-25 21:48:20 +0500

    Deploy push initRangeSlider

--- src/scripts/modules/quizSlider.js
+++ src/scripts/modules/quizSlider.js
@@ -1,30 +1,32 @@
 window.addEventListener('load', () => {
-  const sliderEl = document.querySelector('#range2');
-  const sliderFilter = document.querySelector('.filterRange');
-  const sliderValue = document.querySelector('.rangeValue');
-  const sliderValueFilter = document.querySelector('.rangeValueFilter');
+  function initRangeSlider() {
+    const sliderEl = document.querySelector('#range2');
+    const sliderFilter = document.querySelector('.filterRange');
+    const sliderValue = document.querySelector('.rangeValue');
+    const sliderValueFilter = document.querySelector('.rangeValueFilter');
 
-  if (!sliderEl || !sliderFilter || !sliderValue || !sliderValueFilter) return;
+    if (!sliderEl || !sliderFilter || !sliderValue || !sliderValueFilter) return;
 
-  sliderValue.textContent = '2M';
+    sliderValue.textContent = '2M';
 
-  sliderEl.addEventListener('input', (event) => {
-    const tempSliderValue = event.target.value;
-    sliderValue.textContent = `${tempSliderValue}M`;
+    sliderEl.addEventListener('input', (event) => {
+      const tempSliderValue = event.target.value;
+      sliderValue.textContent = `${tempSliderValue}M`;
 
-    const progress = (tempSliderValue / sliderEl.max) * 100;
+      const progress = (tempSliderValue / sliderEl.max) * 100;
 
-    sliderEl.style.background = `linear-gradient(to right, #6ceabd ${progress}%, #ccc ${progress}%)`;
-  });
+      sliderEl.style.background = `linear-gradient(to right, #6ceabd ${progress}%, #ccc ${progress}%)`;
+    });
 
-  sliderValueFilter.textContent = '2M';
+    sliderValueFilter.textContent = '2M';
 
-  sliderFilter.addEventListener('input', (event) => {
-    const tempSliderValue = event.target.value;
-    sliderValueFilter.textContent = `${tempSliderValue}M`;
+    sliderFilter.addEventListener('input', (event) => {
+      const tempSliderValue = event.target.value;
+      sliderValueFilter.textContent = `${tempSliderValue}M`;
 
-    const progress = (tempSliderValue / sliderFilter.max) * 100;
+      const progress = (tempSliderValue / sliderFilter.max) * 100;
 
-    sliderFilter.style.background = `linear-gradient(to right, #6ceabd ${progress}%, #ccc ${progress}%)`;
-  });
+      sliderFilter.style.background = `linear-gradient(to right, #6ceabd ${progress}%, #ccc ${progress}%)`;
+    });
+  }
 });
