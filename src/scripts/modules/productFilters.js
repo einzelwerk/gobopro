6e19f10ac2d331ddd287c4af7db0b39a98fe8dd9
clutch4t <clutch4t@gmail.com>
2023-07-27 11:57:48 +0500

    Deploy

--- src/scripts/modules/productFilters.js
+++ src/scripts/modules/productFilters.js
@@ -10,9 +10,16 @@ export function initProductsFilters() {
 
       for (let i = 0; i < element.previousElementSibling.children.length; i += 1) {
         element.previousElementSibling.children[i].addEventListener('click', () => {
-          if (!element.previousElementSibling.children[i].children[1].children[0].classList.contains('active')) {
+          if (
+            element.previousElementSibling.children[i].children[1].children[0].closest('.option').querySelector('input')
+              .checked === false
+          ) {
             // eslint-disable-next-line
             element.previousElementSibling.children[i].children[1].children[0].classList.add('active');
+            // eslint-disable-next-line
+            element.previousElementSibling.children[i].children[1].children[0]
+              .closest('.option')
+              .querySelector('input').checked = true;
           }
           // eslint-disable-next-line
           element.innerHTML = element.previousElementSibling.children[i].textContent;
