f6eb78d131e088c88d125f2f2f0486fbc6512dcc
clutch4t <clutch4t@gmail.com>
2023-09-28 19:08:55 +0500

    deploy fixed range slider in quiz

--- src/scripts/modules/burgerMenu.js
+++ src/scripts/modules/burgerMenu.js
@@ -1,27 +1,27 @@
 export function toggleMenu() {
-    const menu = document.querySelector(".menu");
-    const content = document.querySelector(".menu__content");
-    const burgerMenuIcon = document.querySelector('.menu__burger__icon');
+  const menu = document.querySelector('.menu');
+  const content = document.querySelector('.menu__content');
+  const burgerMenuIcon = document.querySelector('.menu__burger__icon');
 
-    if (!menu || !content) return;
+  if (!menu || !content) return;
 
-    menu.addEventListener('click', () => {
-        if (menu.classList.contains('menu_active')) {
-            menu.classList.remove('menu_active');
-            burgerMenuIcon.src = "./assets/images/hamburger-open.svg";
-            content.style.display = "none";
-            content.style.transform = `translateX(-200px)`;
-            document.body.style.overflow = "auto";
-            return;
-        }
-        menu.classList.add('menu_active');
-        burgerMenuIcon.src = "./assets/images/burger-close.svg";
-        content.style.display = "flex";
-        content.style.transform = `translateX(0px)`;
-        document.body.style.overflow = 'hidden';
-    });
+  menu.addEventListener('click', () => {
+    if (menu.classList.contains('menu_active')) {
+      menu.classList.remove('menu_active');
+      burgerMenuIcon.src = './assets/images/hamburger-open.svg';
+      content.style.display = 'none';
+      content.style.transform = `translateX(-200px)`;
+      document.body.style.overflow = 'auto';
+      return;
+    }
+    menu.classList.add('menu_active');
+    burgerMenuIcon.src = './assets/images/burger-close.svg';
+    content.style.display = 'flex';
+    content.style.transform = `translateX(0px)`;
+    document.body.style.overflow = 'hidden';
+  });
 
-    content.addEventListener('click', (event) => {
-        event.stopPropagation();
-    })
+  content.addEventListener('click', (event) => {
+    event.stopPropagation();
+  });
 }
